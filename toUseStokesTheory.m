43783e094524c06a632b6b30786568e237efadaa
KuifengZhao <35133425+KuifengZhao@users.noreply.github.com>
2022-01-15 15:34:17 +0800

    Update toUseStokesTheory.m
    update about how to use the codes, StokesDispSolver + StokesEta + StokesU

--- toUseStokesTheory.m
+++ toUseStokesTheory.m
@@ -18,38 +18,31 @@ elseif modeNo ==2
     a = Result.aw;
     H = Result.Hw;
 end
-% k1 = Result1.k; L1 = Result1.L; % wave number and wave length
-eta = StokesEta(Result.k, h0, a, modeNo, theta0); % free surface
+
+% Below is to call the free surface function StokesEta, make your own edit to select the preferred output. 
+[eta, eta1, eta2, eta3,eta4,eta5] = StokesEta(Result.k, h, Result.a, theta); % free surface
 plot(theta0,eta)
 c = Result.L/T; % phase speed
 
 % velocity and potential function
-for i = 1:length(theta0)    
-z = linspace(-h0, eta(i), 10);
-[phi(:,i), u(:,i), w(:,i)] = StokesU(Result.k, h0, a, modeNo, theta0(i), z);
-% plot(u,z,'ro', w,z, 'kx'),hold on,
-% axis([-4 4 -1 0.4])
-% hwl = refline(0, eta(i)); hwl.Color = 'b'; hwl.LineWidth = 2;
-% pause(0.02)
-% hold off,
+for i = 1:length(theta)    
+% z = [-10:0.5:-1.5, eta(i)];%linspace(-h, eta(i), 10);
+z = [linspace(-h, eta(i), 10), -2.5];% 
+[phi(i,:), u, u1(i,:),u2(i,:),u3(i,:),u4(i,:),u5(i,:), ...
+    w,w1(i,:),w2(i,:),w3(i,:),w4(i,:),w5(i,:)] = StokesU(Result.k, h, Result.a, theta(i), z);
+    
 end
-ResultCase3 = Result;
-[a, H, max(eta), min(eta), max(eta)- min(eta), abs(max(eta)- min(eta) - H)/H]
-etaCase3= eta;
-uCase3 = u;
-wCase3 = w;
+
+Result.u = u;
+Result.w = w;
 
 % etaFen = FentonEta(Result.k, h0, a, theta0)
 
 % % For the second mode, a is combined first harmonic amplitude
 % modeNo = 2; 
 % Result2 =StokesDispSolver('h', h0, 'T', T0, 'a', a,'mode', 2);
-% k2 = Result2.k; L2 = Result2.L;
-% eta = StokesEta(k2, h0, a, 2, theta0);
-% for i = 1:length(theta0)    
-% z = linspace(-h0, eta(i), 10);
-% [phi(:,i), u(:,i), w(:,i)] = StokesU(k2, h0, a, modeNo, theta0(i), z);
-% end
-% % 2. function region
+
+% Then we can use StokesEta and StokesU functions
+
 
 
