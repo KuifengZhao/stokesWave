13892445a3511def4c637fe1476b3392241794df
KuifengZhao <35133425+KuifengZhao@users.noreply.github.com>
2022-01-30 16:37:25 +0800

    Update StokesDispSolver.m

--- StokesDispSolver.m
+++ StokesDispSolver.m
@@ -47,26 +47,26 @@ if and(exist('a', 'var') ==1, exist('T', 'var') ==1)
     L0 = g*T^2/2/pi;
     if modeNo == 1 
         omega2 = k^2*a^2*(2*alpha1^2+7)/4/(alpha1 -1)^2;
-        omega4 = (a^4*k^4*(20*alpha1^6 + 132*alpha1^5 + 20*alpha1^4 ...
-        - 184*alpha1^3 - 243*alpha1^2 - 11*alpha1 + 428))/(32*(alpha1 - 1)^5*(alpha1 + 1));
+        omega4 = (a^4*k^4*(20*alpha1^5 + 112*alpha1^4 ...
+        - 100*alpha1^3 - 68*alpha1^2 - 211*alpha1 + 328))/(32*(alpha1 - 1)^5);
         omegaFun = omega0*(1+omega2+omega4) - omega;  
         kResults = double(vpasolve(omegaFun, k, [0 Inf]));
         Results.a = a;
         sigma = tanh(kResults*h);
-        B31 = (3+8*sigma^2-9*sigma^4)/16/sigma^4;%(cosh(2*h*kResults)^2 + 12*cosh(2*h*kResults) - 7)/(8*(cosh(2*h*kResults) - 1)^2);
-        B51 = (121*cosh(2*h*kResults)^6 + 432*cosh(2*h*kResults)^5 + 543*cosh(2*h*kResults)^4 - 1407*cosh(2*h*kResults)^3 ...
-        - 258*cosh(2*h*kResults)^2 + 2001*cosh(2*h*kResults) - 1108)/(192*(cosh(2*h*kResults) - 1)^5*(cosh(2*h*kResults) + 1));
+        B31 = (3+8*sigma^2-9*sigma^4)/16/sigma^4;
+        B51 = (121*cosh(2*h*kResults)^5 + 263*cosh(2*h*kResults)^4 +376*cosh(2*h*kResults)^3 ...
+        - 1999*cosh(2*h*kResults)^2 + 2509*cosh(2*h*kResults) - 1108)/(192*(cosh(2*h*kResults) - 1)^5);
         Results.aw = a*(1+kResults^2.*a^2*B31+kResults^4.*a^4*B51);
     elseif modeNo == 2 % in case first harmonic wave amplitude aw is given
         syms a0
 
-        B31 = (3+8*sigma^2-9*sigma^4)/16/sigma^4;%(cosh(2*h*kResults)^2 + 12*cosh(2*h*kResults) - 7)/(8*(cosh(2*h*kResults) - 1)^2);
-        B51 = (121*cosh(2*h*k)^6 + 432*cosh(2*h*k)^5 + 543*cosh(2*h*k)^4 - 1407*cosh(2*h*k)^3 ...
-        - 258*cosh(2*h*k)^2 + 2001*cosh(2*h*k) - 1108)/(192*(cosh(2*h*k) - 1)^5*(cosh(2*h*k) + 1));
+        B31 = (3+8*sigma^2-9*sigma^4)/16/sigma^4;
+        B51 = (121*cosh(2*h*k)^5 + 263*cosh(2*h*k)^4 +376*cosh(2*h*k)^3 ...
+        - 1991*cosh(2*h*k)^2 + 2509*cosh(2*h*k) - 1108)/(192*(cosh(2*h*k) - 1)^5);
         a0Fun = a - a0*(1+k^2.*a0^2*B31+k^4.*a0^4*B51);
         omega2 = k^2*a0^2*(2*alpha1^2+7)/4/(alpha1 -1)^2;
-        omega4 = (a0^4*k^4*(20*alpha1^6 + 132*alpha1^5 + 20*alpha1^4 ...
-        - 184*alpha1^3 - 243*alpha1^2 - 11*alpha1 + 428))/(32*(alpha1 - 1)^5*(alpha1 + 1));
+        omega4 = (a0^4*k^4*(20*alpha1^5 + 112*alpha1^4 ...
+        - 100*alpha1^3 - 68*alpha1^2 - 211*alpha1 + 328))/(32*(alpha1 - 1)^5);
         omegaFun = omega0*(1+omega2+omega4) - omega; 
         S = vpasolve([a0Fun , omegaFun], [k, a0],[2*pi/L0,a]);
         kResults = abs(double(S.k));
@@ -77,10 +77,10 @@ if and(exist('a', 'var') ==1, exist('T', 'var') ==1)
     Results.k = kResults;
     Results.omega0 = sqrt(g*kResults*tanh(kResults*h));
     sigma = tanh(kResults*h);
-    B31 = (3+8*sigma^2-9*sigma^4)/16/sigma^4;%(cosh(2*h*kResults)^2 + 12*cosh(2*h*kResults) - 7)/(8*(cosh(2*h*kResults) - 1)^2);
-    B33 = (27-9*sigma^2+9*sigma^4-3*sigma^6)/64/sigma^6; %(cosh(2*h*kResults)^3 + 3*cosh(2*h*kResults)^2 + 3*cosh(2*h*kResults) + 2)/(8*(cosh(2*h*kResults) - 1)^3);  
-    B51 = (121*cosh(2*h*kResults)^6 + 432*cosh(2*h*kResults)^5 + 543*cosh(2*h*kResults)^4 - 1407*cosh(2*h*kResults)^3 ...
-    - 258*cosh(2*h*kResults)^2 + 2001*cosh(2*h*kResults) - 1108)/(192*(cosh(2*h*kResults) - 1)^5*(cosh(2*h*kResults) + 1));
+    B31 = (3+8*sigma^2-9*sigma^4)/16/sigma^4;
+    B33 = (27-9*sigma^2+9*sigma^4-3*sigma^6)/64/sigma^6;   
+    B51 = (121*cosh(2*h*kResults)^5 + 263*cosh(2*h*kResults)^4 +376*cosh(2*h*kResults)^3 ...
+        - 1999*cosh(2*h*kResults)^2 + 2509*cosh(2*h*kResults) - 1108)/(192*(cosh(2*h*kResults) - 1)^5);
     B53 = (57*cosh(2*h*kResults)^7 + 204*cosh(2*h*kResults)^6 - 53*cosh(2*h*kResults)^5 ...
         -782*cosh(2*h*kResults)^4 - 741*cosh(2*h*kResults)^3 - 52*cosh(2*h*kResults)^2 ...
         + 371*cosh(2*h*kResults) + 186)*9/((3*cosh(2*h*kResults) + 2)*(cosh(2*h*kResults) - 1)^6*128);
@@ -98,30 +98,30 @@ elseif and(exist('H', 'var') ==1, exist('T', 'var') ==1)
     L0 = g*T^2/2/pi;
     syms a
      sigma = tanh(k*h);
-        B33 = (27-9*sigma^2+9*sigma^4-3*sigma^6)/64/sigma^6; %(cosh(2*h*k)^3 + 3*cosh(2*h*k)^2 + 3*cosh(2*h*k) + 2)/(8*(cosh(2*h*k) - 1)^3);        
+        B33 = (27-9*sigma^2+9*sigma^4-3*sigma^6)/64/sigma^6;        
         B55 = (300*cosh(2*h*k)^8 + 1579*cosh(2*h*k)^7 + 3176*cosh(2*h*k)^6 + 2949*cosh(2*h*k)^5 ...
             + 1188*cosh(2*h*k)^4 + 675*cosh(2*h*k)^3 + 1326*cosh(2*h*k)^2 + 827*cosh(2*h*k) ...
             + 130)*5/((cosh(2*h*k) - 1)^6*(12*cosh(2*h*k)^2 + 11*cosh(2*h*k) + 2)*384);
-        B31 = (3+8*sigma^2-9*sigma^4)/16/sigma^4;%(cosh(2*h*k)^2 + 12*cosh(2*h*k) - 7)/(8*(cosh(2*h*k) - 1)^2);
-        B51 = (121*cosh(2*h*k)^6 + 432*cosh(2*h*k)^5 + 543*cosh(2*h*k)^4 - 1407*cosh(2*h*k)^3 ...
-            - 258*cosh(2*h*k)^2 + 2001*cosh(2*h*k) - 1108)/(192*(cosh(2*h*k) - 1)^5*(cosh(2*h*k) + 1));
+        B31 = (3+8*sigma^2-9*sigma^4)/16/sigma^4;
+        B51 = (121*cosh(2*h*k)^5 + 263*cosh(2*h*k)^4 +376*cosh(2*h*k)^3 ...
+        - 1999*cosh(2*h*k)^2 + 2509*cosh(2*h*k) - 1108)/(192*(cosh(2*h*k) - 1)^5);
         B53 = (57*cosh(2*h*k)^7 + 204*cosh(2*h*k)^6 - 53*cosh(2*h*k)^5 ...
             -782*cosh(2*h*k)^4 - 741*cosh(2*h*k)^3 - 52*cosh(2*h*k)^2 ...
             + 371*cosh(2*h*k) + 186)*9/((3*cosh(2*h*k) + 2)*(cosh(2*h*k) - 1)^6*128);
         Results.H = H;
         Hfun1 = H -(2*a+2*(B31+B33)*k^2*a^3+2*(B51+B53+B55)*k^4*a^5) ==0;
         omega2 = k^2*a^2*(2*alpha1^2+7)/4/(alpha1 -1)^2;
-        omega4 = (a^4*k^4*(20*alpha1^6 + 132*alpha1^5 + 20*alpha1^4 ...
-            - 184*alpha1^3 - 243*alpha1^2 - 11*alpha1 + 428))/(32*(alpha1 - 1)^5*(alpha1 + 1));
+        omega4 = (a^4*k^4*(20*alpha1^5 + 112*alpha1^4 ...
+        - 100*alpha1^3 - 68*alpha1^2 - 211*alpha1 + 328))/(32*(alpha1 - 1)^5);
         omegaFun1 = omega0*(1+omega2+omega4) - omega ==0;
         S = vpasolve([Hfun1 , omegaFun1], [k, a],[2*pi/L0,H/2]);
         kResults = abs(double(S.k));
         aResults = double(S.a);
         Results.a = aResults;
         sigma = tanh(kResults*h);
-        B31 = (3+8*sigma^2-9*sigma^4)/16/sigma^4;%(cosh(2*h*kResults)^2 + 12*cosh(2*h*kResults) - 7)/(8*(cosh(2*h*kResults) - 1)^2);
-        B51 = (121*cosh(2*h*kResults)^6 + 432*cosh(2*h*kResults)^5 + 543*cosh(2*h*kResults)^4 - 1407*cosh(2*h*kResults)^3 ...
-            - 258*cosh(2*h*kResults)^2 + 2001*cosh(2*h*kResults) - 1108)/(192*(cosh(2*h*kResults) - 1)^5*(cosh(2*h*kResults) + 1));
+        B31 = (3+8*sigma^2-9*sigma^4)/16/sigma^4;
+        B51 = (121*cosh(2*h*kResults)^5 + 263*cosh(2*h*kResults)^4 +376*cosh(2*h*kResults)^3 ...
+        - 1999*cosh(2*h*kResults)^2 + 2509*cosh(2*h*kResults) - 1108)/(192*(cosh(2*h*kResults) - 1)^5);
         Results.aw = aResults*(1+kResults^2.*aResults^2*B31+kResults^4.*aResults^4*B51);
         Results.omega0 = sqrt(g*kResults*tanh(kResults*h));
 elseif  and(exist('a', 'var') ==1, exist('omega0', 'var') ==1)
@@ -129,13 +129,13 @@ elseif  and(exist('a', 'var') ==1, exist('omega0', 'var') ==1)
     kResults = double(vpasolve(omega0^2 - g*k*tanh(k*h), k, [0 inf]));
     alpha1 = cosh(2*kResults*h);   
     sigma = tanh(kResults*h);
-    B33 = (27-9*sigma^2+9*sigma^4-3*sigma^6)/64/sigma^6; %(cosh(2*h*kResults)^3 + 3*cosh(2*h*kResults)^2 + 3*cosh(2*h*kResults) + 2)/(8*(cosh(2*h*kResults) - 1)^3);    
+    B33 = (27-9*sigma^2+9*sigma^4-3*sigma^6)/64/sigma^6;    
     B55 = (300*cosh(2*h*kResults)^8 + 1579*cosh(2*h*kResults)^7 + 3176*cosh(2*h*kResults)^6 + 2949*cosh(2*h*kResults)^5 ...
         + 1188*cosh(2*h*kResults)^4 + 675*cosh(2*h*kResults)^3 + 1326*cosh(2*h*kResults)^2 + 827*cosh(2*h*kResults) ...
         + 130)*5/((cosh(2*h*kResults) - 1)^6*(12*cosh(2*h*kResults)^2 + 11*cosh(2*h*kResults) + 2)*384);
-    B31 = (3+8*sigma^2-9*sigma^4)/16/sigma^4;%(cosh(2*h*kResults)^2 + 12*cosh(2*h*kResults) - 7)/(8*(cosh(2*h*kResults) - 1)^2);
-    B51 = (121*cosh(2*h*kResults)^6 + 432*cosh(2*h*kResults)^5 + 543*cosh(2*h*kResults)^4 - 1407*cosh(2*h*kResults)^3 ...
-        - 258*cosh(2*h*kResults)^2 + 2001*cosh(2*h*kResults) - 1108)/(192*(cosh(2*h*kResults) - 1)^5*(cosh(2*h*kResults) + 1));
+    B31 = (3+8*sigma^2-9*sigma^4)/16/sigma^4;
+    B51 = (121*cosh(2*h*kResults)^5 + 263*cosh(2*h*kResults)^4 +376*cosh(2*h*kResults)^3 ...
+        - 1999*cosh(2*h*kResults)^2 + 2509*cosh(2*h*kResults) - 1108)/(192*(cosh(2*h*kResults) - 1)^5);
     B53 = (57*cosh(2*h*kResults)^7 + 204*cosh(2*h*kResults)^6 - 53*cosh(2*h*kResults)^5 ...
         -782*cosh(2*h*kResults)^4 - 741*cosh(2*h*kResults)^3 - 52*cosh(2*h*kResults)^2 ...
         + 371*cosh(2*h*kResults) + 186)*9/((3*cosh(2*h*kResults) + 2)*(cosh(2*h*kResults) - 1)^6*128);
@@ -147,8 +147,8 @@ elseif  and(exist('a', 'var') ==1, exist('omega0', 'var') ==1)
             Hout = 2*a+2*(B31+B33)*kResults^2*a^3+2*(B51+B53+B55)*kResults^4*a^5;
             Results.H = Hout;
             omega2 = kResults^2*a^2*(2*alpha1^2+7)/4/(alpha1 -1)^2;
-            omega4 = (a^4*kResults^4*(20*alpha1^6 + 132*alpha1^5 + 20*alpha1^4 ...
-                - 184*alpha1^3 - 243*alpha1^2 - 11*alpha1 + 428))/(32*(alpha1 - 1)^5*(alpha1 + 1));
+            omega4 = (a^4*kResults^4*(20*alpha1^5 + 112*alpha1^4 ...
+        - 100*alpha1^3 - 68*alpha1^2 - 211*alpha1 + 328))/(32*(alpha1 - 1)^5);
             omega = omega0*(1+omega2+omega4);
             Results.omega = double(omega);
             Results.T = 2*pi/omega;
@@ -159,8 +159,8 @@ elseif  and(exist('a', 'var') ==1, exist('omega0', 'var') ==1)
             Results.a = double(vpasolve(a0Fun, a0, [0 inf]));
             a = Results.a;
             omega2 = kResults^2*a^2*(2*alpha1^2+7)/4/(alpha1 -1)^2;
-            omega4 = (a^4*kResults^4*(20*alpha1^6 + 132*alpha1^5 + 20*alpha1^4 ...
-                - 184*alpha1^3 - 243*alpha1^2 - 11*alpha1 + 428))/(32*(alpha1 - 1)^5*(alpha1 + 1));
+            omega4 = (a^4*kResults^4*(20*alpha1^5 + 112*alpha1^4 ...
+                - 100*alpha1^3 - 68*alpha1^2 - 211*alpha1 + 328))/(32*(alpha1 - 1)^5);
             omega = omega0*(1+omega2+omega4);
             Results.omega = double(omega);
             Results.T = 2*pi/omega;
@@ -175,13 +175,13 @@ elseif and(exist('H', 'var') ==1, exist('omega0', 'var') ==1)
     alpha1 = cosh(2*kResults*h);
     syms a 
     sigma = tanh(kResults*h);
-    B33 = (27-9*sigma^2+9*sigma^4-3*sigma^6)/64/sigma^6; %(cosh(2*h*kResults)^3 + 3*cosh(2*h*kResults)^2 + 3*cosh(2*h*kResults) + 2)/(8*(cosh(2*h*kResults) - 1)^3);    
+    B33 = (27-9*sigma^2+9*sigma^4-3*sigma^6)/64/sigma^6;     
     B55 = (300*cosh(2*h*kResults)^8 + 1579*cosh(2*h*kResults)^7 + 3176*cosh(2*h*kResults)^6 + 2949*cosh(2*h*kResults)^5 ...
         + 1188*cosh(2*h*kResults)^4 + 675*cosh(2*h*kResults)^3 + 1326*cosh(2*h*kResults)^2 + 827*cosh(2*h*kResults) ...
         + 130)*5/((cosh(2*h*kResults) - 1)^6*(12*cosh(2*h*kResults)^2 + 11*cosh(2*h*kResults) + 2)*384);
-    B31 = (3+8*sigma^2-9*sigma^4)/16/sigma^4;%(cosh(2*h*kResults)^2 + 12*cosh(2*h*kResults) - 7)/(8*(cosh(2*h*kResults) - 1)^2);
-    B51 = (121*cosh(2*h*kResults)^6 + 432*cosh(2*h*kResults)^5 + 543*cosh(2*h*kResults)^4 - 1407*cosh(2*h*kResults)^3 ...
-        - 258*cosh(2*h*kResults)^2 + 2001*cosh(2*h*kResults) - 1108)/(192*(cosh(2*h*kResults) - 1)^5*(cosh(2*h*kResults) + 1));
+    B31 = (3+8*sigma^2-9*sigma^4)/16/sigma^4;
+    B51 = (121*cosh(2*h*kResults)^5 + 263*cosh(2*h*kResults)^4 +376*cosh(2*h*kResults)^3 ...
+        - 1999*cosh(2*h*kResults)^2 + 2509*cosh(2*h*kResults) - 1108)/(192*(cosh(2*h*kResults) - 1)^5);
     B53 = (57*cosh(2*h*kResults)^7 + 204*cosh(2*h*kResults)^6 - 53*cosh(2*h*kResults)^5 ...
         -782*cosh(2*h*kResults)^4 - 741*cosh(2*h*kResults)^3 - 52*cosh(2*h*kResults)^2 ...
         + 371*cosh(2*h*kResults) + 186)*9/((3*cosh(2*h*kResults) + 2)*(cosh(2*h*kResults) - 1)^6*128);              
@@ -189,16 +189,16 @@ elseif and(exist('H', 'var') ==1, exist('omega0', 'var') ==1)
     Hfun1 = H -(2*a+2*(B31+B33)*kResults^2*a^3+2*(B51+B53+B55)*kResults^4*a^5) ==0;
     aResults = double(vpasolve(Hfun1, [0 H]));
     omega2 = kResults^2*aResults^2*(2*alpha1^2+7)/4/(alpha1 -1)^2;
-    omega4 = (aResults^4*kResults^4*(20*alpha1^6 + 132*alpha1^5 + 20*alpha1^4 ...
-        - 184*alpha1^3 - 243*alpha1^2 - 11*alpha1 + 428))/(32*(alpha1 - 1)^5*(alpha1 + 1));
+    omega4 = (aResults^4*kResults^4*(20*alpha1^5 + 112*alpha1^4 ...
+        - 100*alpha1^3 - 68*alpha1^2 - 211*alpha1 + 328))/(32*(alpha1 - 1)^5);
     omega = omega0*(1+omega2+omega4);
     Results.a = aResults;
     Results.omega = omega;
     Results.T = 2*pi/omega;
     sigma = tanh(kResults*h);
-        B31 = (3+8*sigma^2-9*sigma^4)/16/sigma^4;%(cosh(2*h*kResults)^2 + 12*cosh(2*h*kResults) - 7)/(8*(cosh(2*h*kResults) - 1)^2);
-        B51 = (121*cosh(2*h*kResults)^6 + 432*cosh(2*h*kResults)^5 + 543*cosh(2*h*kResults)^4 - 1407*cosh(2*h*kResults)^3 ...
-            - 258*cosh(2*h*kResults)^2 + 2001*cosh(2*h*kResults) - 1108)/(192*(cosh(2*h*kResults) - 1)^5*(cosh(2*h*kResults) + 1));
+        B31 = (3+8*sigma^2-9*sigma^4)/16/sigma^4;
+        B51 = (121*cosh(2*h*kResults)^5 + 263*cosh(2*h*kResults)^4 +376*cosh(2*h*kResults)^3 ...
+        - 1999*cosh(2*h*kResults)^2 + 2509*cosh(2*h*kResults) - 1108)/(192*(cosh(2*h*kResults) - 1)^5);
         Results.aw = aResults*(1+kResults^2.*aResults^2*B31+kResults^4.*aResults^4*B51);
   
 else    
@@ -208,5 +208,3 @@ Results.k = kResults;
 Results.L = 2*pi/kResults;
 Results.c = Results.L/Results.T;
 end
-
-
